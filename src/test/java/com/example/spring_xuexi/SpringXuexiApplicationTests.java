b00528cfd2a112b99531092959b7b2f7b99eda64
wangteng <19931208WTw>
2022-06-30 14:21:40 +0800

    Initial commit

---
+++ src/test/java/com/example/spring_xuexi/SpringXuexiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_xuexi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringXuexiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
