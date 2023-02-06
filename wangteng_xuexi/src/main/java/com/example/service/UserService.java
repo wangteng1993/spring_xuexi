9e1fdde057602e2c41ba6185596cf8970e41c50e
wangteng <2264645267@qq.com>
2023-02-06 09:30:40 +0800

    spring bean 初始化

--- wangteng_xuexi/src/main/java/com/example/service/UserService.java
+++ wangteng_xuexi/src/main/java/com/example/service/UserService.java
@@ -1,13 +1,26 @@
 package com.example.service;
 
+import com.example.bean.User;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
+@Data
 public class UserService {
 
     @Autowired
     OrderService orderService;
+
+    @Autowired
+    private User admin;
+
+   @PostConstruct
+    public void a(){
+//        mysql --->User对象--->admin
+    }
 
     public void test(){
         System.out.println("test");
--- wangteng_xuexi/src/main/java/com/example/test/Test.java
+++ wangteng_xuexi/src/main/java/com/example/test/Test.java
@@ -2,15 +2,40 @@ package com.example.test;
 
 import com.example.config.AppConfig;
 import com.example.service.UserService;
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Test {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         //Bean 对象
-        //UserService.class -->无参构造方法--->对象-------->bean
+        //    UserService.class -->无参构造方法--->对象--
+        //    -->依赖注入( @Autowired 属性赋值)-->
+        //    初始化前(调用a方法)---->初始化--->初始化后--->bean
         UserService userService = (UserService) context.getBean("userService");
+        userService.getAdmin();
+
+
+
+        //@Autowired实现
+        for (Field field : userService.getClass().getFields()) {
+            if (field.isAnnotationPresent(Autowired.class)){
+//                field赋值
+//                field.set(userService,);
+            }
+        }
+        //  @PostConstruct 实现
+        for (Method method : userService.getClass().getMethods()) {
+            if (method.isAnnotationPresent(Constructor.class)){
+
+            }
+        }
+
 
         //new 对象区别 orderService 无值
         UserService userService1 = new UserService();
